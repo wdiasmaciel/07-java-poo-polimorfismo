3ad7256e6980bb2609cc226e724d07fef6e1dab5
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-02 22:27:04 +0000

    Exemplo

---
+++ principal/Principal.java
@@ -0,0 +1,17 @@
+package principal;
+
+import pessoa.Pessoa;
+import pessoa.PessoaFisica;
+
+public class Principal {
+  static int num = 1;
+
+  public static void main(String[] args) {
+    Pessoa pessoa = new Pessoa(Ana);
+    System.out.println(pessoa.getIdentificacao());
+
+    pessoaFisica = new PessoaFisica("Iris", "123.456.789-00");
+    System.out.println(pessoaFisica.getIdentificacao());
+    System.out.println(pessoaFisica.getIdentificacao("(01)234567890"));
+  }
+}
