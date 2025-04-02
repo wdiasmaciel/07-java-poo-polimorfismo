d823a9125221c5572966fd5130af4ccbe46e1ef3
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-02 22:28:19 +0000

    Exemplo

--- principal/Principal.java
+++ principal/Principal.java
@@ -7,10 +7,10 @@ public class Principal {
   static int num = 1;
 
   public static void main(String[] args) {
-    Pessoa pessoa = new Pessoa(Ana);
+    Pessoa pessoa = new Pessoa("Ana");
     System.out.println(pessoa.getIdentificacao());
 
-    pessoaFisica = new PessoaFisica("Iris", "123.456.789-00");
+    PessoaFisica pessoaFisica = new PessoaFisica("Iris", "123.456.789-00");
     System.out.println(pessoaFisica.getIdentificacao());
     System.out.println(pessoaFisica.getIdentificacao("(01)234567890"));
   }
